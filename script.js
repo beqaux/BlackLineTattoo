a06cebd02300b8d320ca3c0b519c38d93c4a14e3
Berkay Kiran <berkaykirannn@gmail.com>
2022-12-17 19:07:53 +0300

    line added
    line şimdilik eklendi

--- script.js
+++ script.js
@@ -56,3 +56,21 @@ function calledEvent(){
 }              
 
 // logo slide script ends here
+
+// // svg code
+// let path = document.querySelector('path')
+//         let pathLength = path.getTotalLength()
+
+//         path.style.strokeDasharray = pathLength + ' ' + pathLength;
+        
+//         path.style.strokeDashoffset = pathLength;
+
+//         window.addEventListener('scroll', () => {
+//             var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+//             var drawLength = pathLength * 3 * scrollPercentage;
+            
+//             path.style.strokeDashoffset = pathLength - drawLength;
+//         })
+
+//         // svg code ends
