f85e42506e32e2ab9c4caedb8de9ab1ce349da54
Berkay Kiran <berkaykirannn@gmail.com>
2022-12-17 16:41:21 +0300

    desktopfinished
    Masaüstünde hiçbi sıkıntı yok ama mobilde küçük ekranda patlıyor.
    
    Karosel ekranı küçültünce menünün altında falan kalıyor

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 //    galeri için active hover scripti starts here
-        $(document).ready(function() {
-          var selectedItem = null;
-      
-          $(".list-item").hover(function() {
-            $(this).css({
-              "background-color": "rgb(32, 32, 32)",
-              "color": "white"
-            });
-          }, function() {
-            if (this != selectedItem) {
-              $(this).css({
-                "background-color": "black",
-                "color": "white"
-              });
-            }
-          });
-      
-          $(".list-item").click(function() {
-            if (selectedItem != null) {
-              $(selectedItem).css({
-                "background-color": "black",
-                "color": "white"
-              });
-            }
-            selectedItem = this;
-          });
-        });
+$(document).ready(function() {
+  var selectedItem = null;
+  $(".list-item").hover(function() {
+    $(this).css({
+      "background-color": "rgb(32, 32, 32)",
+      "color": "white"
+    });
+  }, function() {
+    if (this != selectedItem) {
+      $(this).css({
+        "background-color": "black",
+        "color": "white"
+      });
+    }
+  });
+  $(".list-item").click(function() {
+    if (selectedItem != null) {
+      $(selectedItem).css({
+        "background-color": "black",
+        "color": "white"
+      });
+    }
+    selectedItem = this;
+  });
+});
     
-        // galeri için active hover scripti ends here
+    // galeri için active hover scripti ends her
+    // galeri için kategori scripti starts here
+    $(document).ready(function() {
+        $("#categories li").click(function() {
+          var category = $(this).data("category");
+          $("#photos img").hide();
+          $("#photos img[data-category-" + category + "='true']").show();
+        });
+      })
 
 
-        // galeri için kategori scripti starts here
-        $(document).ready(function() {
-            $("#categories li").click(function() {
-              var category = $(this).data("category");
-              $("#photos img").hide();
-              $("#photos img[data-category-" + category + "='true']").show();
-            });
-          });
-
-        //   galeri için kategori scripti ends here
-
-        // deneme
-       
-            // $('#logo').on('scroll', function() {
-            //     if ($(this).is(':in-viewport')) {
-            //       // The element is in view, so change its class name
-            //       $(this).addClass('main py-5 sticky-top');
-            //     } else {
-            //       // The element is not in view, so remove the new class name
-            //       $(this).removeClass('main py-5');
-            //     }
-            //   });
-            $(document).ready(function() {
-                $("#main").mouseenter(function() {
-                  $("#logo").removeClass("main py-5 sticky-top");
-                });
-              });
-         
+let called = false
+document.addEventListener('scroll', e => {
+  if (document.documentElement.scrollTop >= 700) {
+    if (called) return
+    called = true
+    calledEvent()
+  }
+})
+function calledEvent(){       
+  window.addEventListener('scroll', function() {
+    var fixedImage = document.querySelector('.fixed-image');
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    fixedImage.style.top = 700 -scrollTop + 'px';
+  });
+}              
